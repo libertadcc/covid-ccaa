2fccb7b357850cca13755c57e4c9739b92a733af
libertadcc <libertadchapinalcervantes@gmail.com>
2021-01-27 14:30:04 +0100

    ADD popup with data

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ require([
     "esri/Map",
     "esri/views/MapView",
     "esri/layers/FeatureLayer",
-    "esri/Graphic"
-], function(Map, MapView, FeatureLayer, Graphic) {
+    "esri/Graphic",
+    "esri/layers/GraphicsLayer",
+], function(Map, MapView, FeatureLayer, Graphic, GraphicsLayer) {
 
     var fields = [
         {
@@ -53,7 +54,6 @@ require([
         }
     ];
 
-
     map = new Map({
         basemap: "dark-gray-vector"
     });
@@ -62,29 +62,37 @@ require([
         container: "viewDiv",
         map: map,
         center: [-3.7197, 40.41304],
-        zoom: 5
+        zoom: 5,
+        constraints: {
+          rotationEnabled: false
+        }
     });
-
+    let layer ;
     view.when(function() {
         fetch('./data/vaccines_latest.js')
         .then(response => response.json())
         .then(data => {
             let geodata = geolocateCovidData(data).filter(data => data != undefined);
-
-            let layer = new FeatureLayer({
+            layer = new FeatureLayer({
                 source: geodata,
                 fields: fields,
-                objectIdField: "ObjectID",
-                popupTemplate: {
-                    title: "HOLA"
-                },
+                objectIdField: "ObjectID"
             });
             map.add(layer);
+            return layer;
+        })
+        .then((layer) => {
+            canaryView.on("pointer-move", function(event) {  
+                return highlightFeature(layer, canaryView.toMap(event));
+            });
+            view.on("pointer-move", function(event) {  
+                return highlightFeature(layer, view.toMap(event));
+            });
+            
         });
     });
 
-    
-    var canaryDiv = new MapView({
+    var canaryView = new MapView({
         container: "canaryDiv",
         map: map,
         extent: {
@@ -98,9 +106,95 @@ require([
         },
         ui: {
           components: []
+        },
+        constraints: {
+          rotationEnabled: false
         }
     });
     view.ui.add("canaryDiv", "bottom-left");
+
+
+    view.when(disableZooming);
+    canaryView.when(disableZooming);
+
+    function disableZooming(view) {
+
+        view.popup.actions = [];
+        function stopEvtPropagation(event) {
+          event.stopPropagation();
+        }
+
+        view.on("mouse-wheel", stopEvtPropagation);
+        view.on("double-click", stopEvtPropagation);
+        view.on("double-click", ["Control"], stopEvtPropagation);
+        view.on("drag", stopEvtPropagation);
+        view.on("drag", ["Shift"], stopEvtPropagation);
+        view.on("drag", ["Shift", "Control"], stopEvtPropagation);
+        view.on("key-down", function(event) {
+          const prohibitedKeys = ["+", "-", "Shift", "_", "=", "ArrowUp", "ArrowDown", "ArrowRight", "ArrowLeft"];
+          const keyPressed = event.key;
+          if (prohibitedKeys.indexOf(keyPressed) !== -1) {
+            event.stopPropagation();
+          }
+        });
+        return view;
+    };
+
+    const graphicsLayer = new GraphicsLayer({
+        title: "layer"
+    });
+
+    map.add(graphicsLayer);
+
+
+    function highlightFeature(lview, mappoint) {
+        const query = {
+            geometry: mappoint,
+            returnGeometry: true,
+            outFields: ["*"]
+        };
+
+        lview.queryFeatures(query).then((response) => {
+            graphicsLayer.graphics.removeAll();
+           
+
+            if(response.features.length > 0) {
+                const symbol = {
+                  type: "simple-fill",
+                  color: "rgba(0, 255, 255, 1)",
+                  outline: null
+                };
+                var feature = response.features[0];
+                feature.symbol = symbol;
+                graphicsLayer.graphics.add(feature);
+                feature.popupTemplate = {
+                    "title": "{ccaa}",
+                    "content": `<b>Dosis entregadas:</b> {dosisEntregadas}<br>
+                                <b>Dosis entregadas Pfizer:</b> {dosisEntregadasPfizer}<br>
+                                <b>Dosis entregadas Moderna:</b> {dosisEntregadasModerna}<br>
+                                <b>Dosis administratadas:</b> {dosisAdministradas}<br>
+                                <b>Porcentaje entregadas:</b> {porcentajeEntregadas}<br>
+                                <b>Dosis pauta completada:</b> {dosisPautaCompletada}<br>
+                                <b>Porcentaje población totalmente vacunada:</b> {porcentajePoblacionCompletas}<br>`
+                };
+                if (feature.attributes.ccaa === "Canarias") {      
+                    view.popup.close();  
+                    canaryView.popup.open({
+                        features:response.features,
+                        location:mappoint
+                    });
+                    
+                } else {
+                    canaryView.popup.close();
+                    view.popup.open({
+                        features:response.features,
+                        location:mappoint
+                    });
+                    
+                }
+            }
+        });        
+    };
 
     function geolocateCovidData(data) {
 
